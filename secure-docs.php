e6ef5f1237ac5efd10fe1a0ab7d56ceb6e7ae7a6
Gyssak <danielkas2007@gmail.com>
2026-04-15 14:32:06 +0200

    strict types and hooks

--- secure-docs.php
+++ secure-docs.php
@@ -1,2 +1,36 @@
-
 <?php
+
+declare(strict_types=1);
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class SecureDocuments
+{
+    public function __construct()
+    {
+        add_action('init', [$this, 'register_cpt']);
+        add_action('add_meta_boxes', [$this, 'add_meta_box']);
+        add_action('wp_ajax_generate_secure_link', [$this, 'generate_link']);
+        add_action('template_redirect', [$this, 'handle_access']);
+    }
+
+    public function register_cpt(): void
+    {
+    }
+
+    public function add_meta_box(): void
+    {
+    }
+
+    public function generate_link(): void
+    {
+    }
+
+    public function handle_access(): void
+    {
+    }
+}
+
+new SecureDocuments();
